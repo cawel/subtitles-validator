c613e909707de8d0ae49ad6fc20b1a2fb678ab0a
Martin Carel <martin.carel@gmail.com>
2012-02-23 11:22:23 -0500

    verified overlapping

--- README.markdown
+++ README.markdown
@@ -1,7 +1,9 @@
-# Subtitles validator
+# Subtitles-validator
+## In a nutshell
 This (very simple) script validates a few rules for a subtitle file:
 
-* each subtitle in the file should have its start marker set before the end marker.
+* each subtitle should have its start marker set before the end marker.
+* each subtitle should start after the end of the previous one.
 
 
 ## Supported subtitle file formats
--- validate.rb
+++ validate.rb
@@ -13,15 +13,22 @@ end
 
 puts "Starting..."
 
+previous_stop = "00:00:00,000" # smallest possible value
+
 File.open(ARGV[0]) do |file|
 	while true do
 		id, time, _, _ = file.gets, file.gets, file.gets, file.gets
 		break if id.nil? # reached EOF
 
-		start, stop = time.split(' --> ')
+		start, stop = time.split(' --> ').map(&:strip)
 		if start > stop
 			puts "subtitle ##{id.strip} is invalid: #{time}"
 		end
+
+		if previous_stop > start
+			puts "subtitle ##{id.strip} is overlapping with the previous one."
+		end
+		previous_stop = stop
 	end
 end
 
